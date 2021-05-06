25b4052bd88d6327e6fb2757fb0543177d07fa54
jonewonLEE <ljayg@pulipinc.com>
2021-05-06 11:17:54 +0900

    메인페이지 작업

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,15 +1,62 @@
 <template>
-  <footer>
-    <h1>footer</h1>
+  <footer class="footer">
+    <div class="foot-inner">
+      <h5><span>THE SHOPS AT CENTERFIELD</span></h5>
+      <div class="foot-wrap">
+        <ul class="addr-info">
+          <li>
+            <div class="tit">더샵스엣센터필드</div>
+            <div>서울시 강남구 테헤란로 231</div>
+          </li>
+          <li>
+            <div class="tit">스타필드</div>
+          </li>
+          <li>
+            <div class="tit">회사소개</div>
+            <div>신세계프라퍼티</div>
+          </li>
+          <li>
+            <div class="tit">협력사상담</div>
+            <a href="#">상담신청 및 입점 안내</a>
+          </li>
+          <li>
+            <div class="familysite">
+              <a href="#" class="select"><span>패밀리사이트</span></a>
+            </div>
+          </li>
+        </ul>
+        <ul class="foot-menu">
+          <li><a href="#">이용약관</a></li>
+          <li><a href="#" class="privacy">개인정보처리방침</a></li>
+          <li>
+            <a href="#">
+              영상정보저리기기<br />
+              운영관리 방침
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul class="sns-menu">
+        <li>
+          <a href="#" class="insta"><span>instagram</span></a>
+        </li>
+        <li>
+          <a href="#" class="youtube"><span>youtube</span></a>
+        </li>
+        <li>
+          <a href="#" class="facebook"><span>facebook</span></a>
+        </li>
+      </ul>
+      <p class="copyright">Copyright &copy; 2021 CENTERFIELD. All Rights Reserved.</p>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'Header'
-}
+  name: 'Header',
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
