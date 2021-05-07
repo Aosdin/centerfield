52debdcaa923a6f86426a28e788b5eeb33ca2adb
jonewonLEE <ljayg@pulipinc.com>
2021-05-07 14:35:36 +0900

    매장상세페이지 완료

--- src/pages/TenantDetail.vue
+++ src/pages/TenantDetail.vue
@@ -6,18 +6,25 @@
       <div class="tenant-info">
         <div class="tenant-tit">FIRE BELL</div>
         <div class="tenant-detail">
-          <div>
+          <div class="subinfo">
             <div class="category">브런치 / 버거 / 샌드위치</div>
             <div class="time">매일 오전 10:00 ~ 오후 9:00</div>
-            <div class="area"></div>
+            <div class="area">EAST TOWER 1F</div>
           </div>
           <button type="button" class="btntel"><span>전화걸기</span></button>
         </div>
-        <p>것이 원질이 이상 끓는 설레는 뿐이다. 더운지라 내려온 꽃이 장식하는 사라지지 청춘은 약동하다. 같으며, 미묘한 장식하는 생의 우리의 소담스러운 웅대한 얼음에 것이다. 것이 아니한 인도하겠다는 보라. 심장의 끓는 이상은 목숨이 새가 광야에서 만물은 것이다. 이는 찬미를 것은 있는가? 황금시대의 있는 이상, 맺어, 바이며, 그것은 말이다.</p>
+        <p class="description">것이 원질이 이상 끓는 설레는 뿐이다. 더운지라 내려온 꽃이 장식하는 사라지지 청춘은 약동하다. 같으며, 미묘한 장식하는 생의 우리의 소담스러운 웅대한 얼음에 것이다. 것이 아니한 인도하겠다는 보라. 심장의 끓는 이상은 목숨이 새가 광야에서 만물은 것이다. 이는 찬미를 것은 있는가? 황금시대의 있는 이상, 맺어, 바이며, 그것은 말이다.</p>
       </div>
       <div class="tenant-location">
         <img src="../assets/images/tenant/tenant_location_firebell.svg" alt="" />
       </div>
+      <!-- 매장 썸네일(s) -->
+      <div class="inner">
+        <div class="tenant-thumb"><img src="../assets/images/tenant/Rectangle_116.png" alt="" /></div>
+        <div class="tenant-thumb"><img src="../assets/images/tenant/Rectangle_117.png" alt="" /></div>
+        <div class="tenant-thumb"><img src="../assets/images/tenant/Rectangle_116.png" alt="" /></div>
+      </div>
+      <!--// 매장 썸네일(e) -->
     </section>
   </div>
 </template>
