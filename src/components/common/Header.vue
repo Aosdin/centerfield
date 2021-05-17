2819f3f95602ae1f07068ad60da6317f35a80484
jonewonLEE <ljayg@pulipinc.com>
2021-05-17 14:16:46 +0900

    마이페이지 수정

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -3,31 +3,28 @@
     <h1>
       <router-link :to="{ name: 'main' }"><span>THE SHOPS AT CENTERFIELD</span></router-link>
     </h1>
-    <a
-      href="javascript:;"
-      class="btn-gnb"
-      @click="showNavi"
-    ><span>메뉴열기</span></a>
+    <a href="javascript:;" class="btn-gnb" @click="showNavi"><span>메뉴열기</span></a>
+    <a href="javascript:;" class="btn-spot"><span></span></a>
   </header>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'Header',
   computed: {
     ...mapState('common', {
-      isNavigation: state => state.isNavigation
-    })
+      isNavigation: state => state.isNavigation,
+    }),
   },
   methods: {
     ...mapActions('common', {
-      setNavigation: 'setNavigation'
+      setNavigation: 'setNavigation',
     }),
-    showNavi () {
-      this.setNavigation(!this.isNavigation)
-    }
-  }
-}
+    showNavi() {
+      this.setNavigation(!this.isNavigation);
+    },
+  },
+};
 </script>
--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -1,52 +1,42 @@
 <template>
   <div class="contents">
     <section class="sector sect-mypage">
-      <div class="inner2">
+      <div class="inner2 ">
         <h3 class="sectit1">MY PAGE</h3>
+        <a href="#" class="btn2 btg-black">개인정보수정</a>
+        <div class="userwrap">
+          <div class="icon"></div>
+          <div class="username"><span class="ftc-primary">김센터</span> 님</div>
+        </div>
       </div>
       <div class="inner">
         <ul class="mypage-list">
-          <li>
-            <div class="item">
-              <h4>회원등급</h4>
-              <div class="item-area">
-                <div class="icon">
-                  <div class="ico-img"></div>
-                </div>
-                <div class="item-tit type1"><span class="ftc-primary">일반</span> 고객</div>
-              </div>
-              <a href="#" class="btn1 btg-primary">더보기</a>
-            </div>
-          </li>
-          <li>
-            <div class="item">
-              <h4>단골점포</h4>
-              <div class="item-area">
-                <div class="item-tit"><span class="ftc-primary">고양점</span></div>
-              </div>
-              <a href="#" class="btn1 btg-primary">더보기</a>
-            </div>
-          </li>
-          <li>
+          <li class="grid-2">
             <div class="item">
               <h4>내 쿠폰함</h4>
               <div class="item-area">
                 <div class="item-tit"><span class="ftc-primary">00</span>개</div>
               </div>
-              <a href="#" class="btn1 btg-primary">더보기</a>
+              <a href="#" class="btn2 btg-primary">더보기</a>
+            </div>
+          </li>
+          <li class="grid-2">
+            <div class="item">
+              <h4>단골점포</h4>
+              <div class="item-area">
+                <div class="item-tit"><span class="ftc-primary">고양점</span></div>
+              </div>
+              <a href="#" class="btn2 btg-primary">더보기</a>
             </div>
           </li>
           <li>
             <div class="item">
-              <h4>신세계포인트</h4>
+              <h4 class="taleft">신세계포인트</h4>
               <div class="item-area">
-                <div class="item-tit"><span class="ftc-primary">2,234</span> Point</div>
-                <p>
-                  현재 사용 가능한 포인트입니다.<br />
-                  다른 가맹점에서 적립/사용한 전역 내역조회는 www.shinsegaepoint.com에서 확인 가능합니다.
-                </p>
+                <div class="item-tit taleft"><span class="ftc-primary">2,003</span></div>
               </div>
-              <a href="#" class="btn1 btg-primary">신세계포인트 조회</a>
+              <div class="barcode"><img src="../assets/images/temp/barcode.png" alt="" /></div>
+              <a href="#" class="btn2 btg-primary">포인트 조회</a>
             </div>
           </li>
           <li>
@@ -59,7 +49,7 @@
                   My 신세계포인트 &gt; My 회원정보에서 가능합니다.
                 </p>
               </div>
-              <a href="#" class="btn1 btg-primary">개인정보수정</a>
+              <a href="#" class="btn2 btg-primary">개인정보수정</a>
               <div class="btn-withdrawal">
                 <a href="#"><span>탈퇴하기</span></a>
               </div>
